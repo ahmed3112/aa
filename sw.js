77701d784ab3b4637f336b511b75636bb6440c3f
AHMED MONTASER <ahmedmontaser4622@gmail.com>
2026-02-28 15:56:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
+const CACHE_NAME="quran-cache-v1";
+const urlsToCache=["./","index.html"];
 
+self.addEventListener("install",e=>{
+ e.waitUntil(
+  caches.open(CACHE_NAME)
+  .then(cache=>cache.addAll(urlsToCache))
+ );
+});
+
+self.addEventListener("fetch",e=>{
+ e.respondWith(
+  caches.match(e.request)
+  .then(r=>r||fetch(e.request))
+ );
+});
